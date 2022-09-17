0ebc222b021ff8339d2e56f4cc23395a8d0111c5
jose-2307 <joseluisflores2307@gmail.com>
2022-09-17 14:10:10 -0300

    backend structuring

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const routerApi = require("./routes");
+
+//const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require("./middlewares/error.handler");
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const whitelist = [""];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido"));
+    }
+  }
+}
+app.use(cors(options));
+
+require("./utils/auth");
+
+routerApi(app);
+
+// app.use(logErrors);
+// app.use(ormErrorHandler);
+// app.use(boomErrorHandler);
+// app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Escuchando en el puerto ${port}`);
+});

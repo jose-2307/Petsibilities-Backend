2a323544f6408fdba0165dd83d3b7525930ea39f
jose-2307 <joseluisflores2307@gmail.com>
2022-09-23 22:16:28 -0300

    user endpoints finished

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const roleRouter = require("./role.router");
+const regionRouter = require("./region.router");
+const cityRouter = require("./city.router");
+const userRouter = require("./user.router");
+
 
 function routerApi(app) {
   const router = express.Router();
   app.use("/api/v1",router);
   router.use("/roles",roleRouter);
+  router.use("/regions",regionRouter);
+  router.use("/cities",cityRouter);
+  router.use("/users",userRouter);
 }
 
 module.exports = routerApi;
---
+++ routes/user.router.js
@@ -0,0 +1,79 @@
+const express = require("express");
+
+
+const UserService = require("./../services/user.service");
+const validatorHandler = require("./../middlewares/validator.handler");
+const { createUserSchema,updateUserSchema,getUserSchema } = require("./../schemas/user.schema");
+const { checkRole } = require("./../middlewares/auth.handler");
+
+const router = express.Router();
+const service = new UserService();
+
+router.get("/",
+  async (req, res, next) => {
+    try {
+      const users = await service.find();
+      res.json(users);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.get("/:id",
+  validatorHandler(getUserSchema, "params"),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await service.findOne(id);
+      res.json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post("/",
+  validatorHandler(createUserSchema, "body"),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newUser = await service.create(body);
+      res.status(201).json(newUser);
+    } catch (error) {
+      next(error)
+    }
+  }
+);
+
+router.patch("/:id",
+  validatorHandler(getUserSchema, "params"),
+  validatorHandler(updateUserSchema, "body"),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const user = await service.update(id,body);
+      res.json(user);
+    } catch (error) {
+      next(error)
+    }
+  }
+);
+
+router.delete("/:id",
+  validatorHandler(getUserSchema, "params"),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.json({id});
+    } catch (error) {
+      next(error)
+    }
+  }
+);
+
+module.exports = router;
+
+
